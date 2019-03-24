7a426b792c1e373ebab11917e8019087cdb4340b
Joel Sotelo <joel.pilgrim88@gmail.com>
2019-03-23 18:26:29 -0600

    identation

--- static/js/app.js
+++ static/js/app.js
@@ -1,73 +1,73 @@
 function buildMetadata(sample) {
 
-  // @TODO: Complete the following function that builds the metadata panel
+	// @TODO: Complete the following function that builds the metadata panel
 
-  // Use `d3.json` to fetch the metadata for a sample
-    // Use d3 to select the panel with id of `#sample-metadata`
+	// Use `d3.json` to fetch the metadata for a sample
+	// Use d3 to select the panel with id of `#sample-metadata`
 
-    // Use `.html("") to clear any existing metadata
+	// Use `.html("") to clear any existing metadata
 
-    // Use `Object.entries` to add each key and value pair to the panel
-    // Hint: Inside the loop, you will need to use d3 to append new
-    // tags for each key-value in the metadata.
+	// Use `Object.entries` to add each key and value pair to the panel
+	// Hint: Inside the loop, you will need to use d3 to append new
+	// tags for each key-value in the metadata.
 
-    // BONUS: Build the Gauge Chart
-    // buildGauge(data.WFREQ);
+	// BONUS: Build the Gauge Chart
+	// buildGauge(data.WFREQ);
 }
 
 function displayMetaData(metaResponse) {
-  let displayPanel = document.getElementById('displayMeta');
-  displayPanel.innerHTML = '';
-  
-  for ( var key in metaResponse) {
-      info = document.createElement("h5");
-      infoText = document.createTextNode(`${key}: ${metaResponse[key]}`);
-      info.append(infoText);
-      displayPanel.appendChild(info);
-      };
+	let displayPanel = document.getElementById('displayMeta');
+	displayPanel.innerHTML = '';
+	
+	for ( var key in metaResponse) {
+			info = document.createElement("h5");
+			infoText = document.createTextNode(`${key}: ${metaResponse[key]}`);
+			info.append(infoText);
+			displayPanel.appendChild(info);
+			};
 }
 
 
 
 function buildCharts(sample) {
-  
-  d3.json("/samples/"+sample).then((sampleNames) => {
-     
-    // console.log(sampleNames);
-   
-    pie_chart(sampleNames);
-    gauge_plot(sampleNames);
-    scatter_plot(sampleNames);
-    
-  }
-  );
+	
+	d3.json("/samples/"+sample).then((sampleNames) => {
+		 
+		// console.log(sampleNames);
+	 
+		pie_chart(sampleNames);
+		gauge_plot(sampleNames);
+		scatter_plot(sampleNames);
+		
+	}
+	);
 
 }
 
 function init() {
-  // Grab a reference to the dropdown select element
-  var selector = d3.select("#selDataset");
+	// Grab a reference to the dropdown select element
+	var selector = d3.select("#selDataset");
 
-  // Use the list of sample names to populate the select options
-  d3.json("/names").then((sampleNames) => {
-    sampleNames.forEach((sample) => {
-      selector
-        .append("option")
-        .text(sample)
-        .property("value", sample);
-    });
+	// Use the list of sample names to populate the select options
+	d3.json("/names").then((sampleNames) => {
+		sampleNames.forEach((sample) => {
+			selector
+				.append("option")
+				.text(sample)
+				.property("value", sample);
+		});
 
-    // Use the first sample from the list to build the initial plots
-    const firstSample = sampleNames[0];
-    buildCharts(firstSample);
-    buildMetadata(firstSample);
-  });
+		// Use the first sample from the list to build the initial plots
+		const firstSample = sampleNames[0];
+		buildCharts(firstSample);
+		buildMetadata(firstSample);
+	});
 }
 
 function optionChanged(newSample) {
-  // Fetch new data each time a new sample is selected
-  buildCharts(newSample);
-  buildMetadata(newSample);
+	// Fetch new data each time a new sample is selected
+	buildCharts(newSample);
+	buildMetadata(newSample);
 }
 
 // Initialize the dashboard
